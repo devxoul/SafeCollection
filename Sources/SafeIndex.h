063ae57468aba96657e919883d6eecb3a4c2d510
Suyeol Jeon <devxoul@gmail.com>
2016-02-18 18:01:42 +0900

    Add Xcode project

---
+++ Sources/SafeIndex.h
@@ -0,0 +1,16 @@
+//
+//  SafeIndex.h
+//  SafeIndex
+//
+//  Created by 전수열 on 2/18/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#if defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR) || defined(TARGET_OS_MAC)
+
+@import Foundation;
+
+FOUNDATION_EXPORT double SafeIndexVersionNumber;
+FOUNDATION_EXPORT const unsigned char SafeIndexVersionString[];
+
+#endif
